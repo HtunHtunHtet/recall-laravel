57850c6799334b702b12ef23199b88d84bd8ccdf
Htun Htun Htet <ryanhhh91@gmail.com>
2025-07-11 17:34:26 -0600

    add jobs

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,8 +18,51 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/about', function () {
-    return view('about');
+Route::get('/jobs', function () {
+    return view('jobs',[
+        'jobs' => [
+            [
+                'id' => 1,
+                'title' => 'Director',
+                'salary' => '$ 50,000'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Programmer',
+                'salary' => '$ 30,000'
+            ],
+            [
+                'id' => 3,
+                'title' => 'Teacher',
+                'salary' => '$ 40,000'
+            ],
+        ]
+    ]);
+});
+
+Route::get('/job/{id}', function ($id) {
+
+    $jobs = [
+        [
+            'id' => 1,
+            'title' => 'Director',
+            'salary' => '$ 50,000'
+        ],
+        [
+            'id' => 2,
+            'title' => 'Programmer',
+            'salary' => '$ 30,000'
+        ],
+        [
+            'id' => 3,
+            'title' => 'Teacher',
+            'salary' => '$ 40,000'
+        ],
+    ];
+
+    return view('job', [
+        'job' => Arr::first($jobs, fn($job) =>  $job['id'] == $id )
+    ]);
 });
 
 Route::get('/contact', function () {
