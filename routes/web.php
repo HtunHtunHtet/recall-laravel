273607a39e33186e3f8930fac3368604bc9cbc4b
Htun Htun Htet <ryanhhh91@gmail.com>
2025-07-13 20:06:01 -0600

    Updates

--- routes/web.php
+++ routes/web.php
@@ -20,17 +20,33 @@ Route::get('/', function () {
 
 Route::get('/jobs', function ()  {
 
-    $job = Job::with('employer')->paginate(3);
+    $job = Job::with('employer')->latest()->paginate(3);
 
-    return view('jobs',[
+    return view('jobs/index',[
         'jobs' => $job
     ]);
 });
 
+Route::get('/job/create', function () {
+   return view('jobs.create');
+});
+
 Route::get('/job/{id}', function ($id) {
-    return view('job', [
+    return view('jobs.show', [
         'job' => Job::find($id)
     ]);
+});
+
+Route::post('/jobs', function (){
+
+
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1
+    ]);
+
+    return redirect('jobs');
 });
 
 Route::get('/contact', function () {
